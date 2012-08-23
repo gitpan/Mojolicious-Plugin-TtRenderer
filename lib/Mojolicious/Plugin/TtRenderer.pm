8cab73f6bab3548e4aa56561e63b5b29828c1db8
Graham Ollis <plicease@cpan.org>
2012-08-23 19:27:53 +0000

    Import of PLICEASE/Mojolicious-Plugin-TtRenderer-1.23 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-TtRenderer
    gitpan-cpan-version:      1.23
    gitpan-cpan-path:         PLICEASE/Mojolicious-Plugin-TtRenderer-1.23.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/TtRenderer.pm
+++ lib/Mojolicious/Plugin/TtRenderer.pm
@@ -1,6 +1,6 @@
 package Mojolicious::Plugin::TtRenderer;
 {
-  $Mojolicious::Plugin::TtRenderer::VERSION = '1.22';
+  $Mojolicious::Plugin::TtRenderer::VERSION = '1.23';
 }
 
 use strict;
--- lib/Mojolicious/Plugin/TtRenderer/Engine.pm
+++ lib/Mojolicious/Plugin/TtRenderer/Engine.pm
@@ -1,6 +1,6 @@
 package Mojolicious::Plugin::TtRenderer::Engine;
 {
-  $Mojolicious::Plugin::TtRenderer::Engine::VERSION = '1.22';
+  $Mojolicious::Plugin::TtRenderer::Engine::VERSION = '1.23';
 }
 
 use warnings;
@@ -90,22 +90,7 @@ sub _render {
     my $ok = $self->tt->process(defined $inline ? \$inline : $t, @params);
 
     # Error
-    unless ($ok) {
-
-        state $rendering_exception;
-
-        my $e = Mojo::Exception->new($self->tt->error.'');
-        $$output = '';
-        $c->app->log->error(qq/Template error in "$t": $e/);
-
-        unless($rendering_exception)
-        {
-            $rendering_exception = 1;
-            $c->render_exception($e);
-            $rendering_exception = 0;
-            $self->tt->error('');
-        }
-    }
+    die $self->tt->error unless $ok;
 
     return 1;
 }
