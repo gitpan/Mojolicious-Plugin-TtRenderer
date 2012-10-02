81e514759f9a21256791f9a02f5d95d058e1e3c6
Graham Ollis <plicease@cpan.org>
2012-10-02 15:43:47 +0000

    Import of PLICEASE/Mojolicious-Plugin-TtRenderer-1.27 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-TtRenderer
    gitpan-cpan-version:      1.27
    gitpan-cpan-path:         PLICEASE/Mojolicious-Plugin-TtRenderer-1.27.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/TtRenderer.pm
+++ lib/Mojolicious/Plugin/TtRenderer.pm
@@ -1,10 +1,11 @@
 package Mojolicious::Plugin::TtRenderer;
 {
-  $Mojolicious::Plugin::TtRenderer::VERSION = '1.26';
+  $Mojolicious::Plugin::TtRenderer::VERSION = '1.27';
 }
 
 use strict;
 use warnings;
+use v5.10;
 
 use base 'Mojolicious::Plugin';
 
@@ -21,10 +22,12 @@ sub register {
     $app->renderer->add_handler(tt => $tt);
 }
 
-local ($Mojolicious::Plugin::TtRenderer::VERSION) = ('devel') unless defined $Mojolicious::Plugin::TtRenderer::VERSION;
+$Mojolicious::Plugin::TtRenderer::VERSION //= ('devel');
 
 1;
 __END__
+
+=encoding utf-8
 
 =head1 NAME
 
@@ -42,7 +45,8 @@ Mojolicious::Plugin::TtRenderer - Template Renderer Plugin
 
 =head1 DESCRIPTION
 
-L<Mojolicous::Plugin::TtRenderer> is a simple loader for L<MojoX::Renderer::TT>.
+L<Mojolicous::Plugin::TtRenderer> is a simple loader for 
+L<Mojolicious::Plugin::TtRenderer::Engine>.
 
 =head1 METHODS
 
@@ -64,5 +68,52 @@ The current controller instance can be accessed as C<c>.
 =head1 SEE ALSO
 
 L<Mojolicious::Plugin::TtRenderer::Engine>, L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+
+=head1 AUTHOR
+
+Current maintainer: Graham Ollis C<< <plicease@cpan.org> >>
+
+Original author: Ask Bjørn Hansen, C<< <ask at develooper.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the project's github issue tracker
+L<https://github.com/abh/mojox-renderer-tt/issues?state=open>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Mojolicious::Plugin::TtRenderer
+
+You can also look for information at:
+
+=over 4
+
+=item * git repository
+
+L<http://git.develooper.com/?p=MojoX-Renderer-TT.git;a=summary>,
+L<git://git.develooper.com/MojoX-Renderer-TT.git>
+
+L<http://github.com/abh/mojox-renderer-tt/>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MojoX-Renderer-TT>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MojoX-Renderer-TT/>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2010 Ask Bjørn Hansen, all rights reserved.
+
+Copyright 2012 Graham Ollis.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 =cut
